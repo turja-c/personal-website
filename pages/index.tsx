de85f1afa481985d657447bf51f23c3cddd8cce4
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-08-08 00:41:06 -0400

    structure change

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ const Home: NextPage = () => {
       
       <HomeBanner />
       {/* <BlogList /> */}
-      <HomeExperience />
       <HomeBlogs />
       <HomeProjects />
+      <HomeExperience />
       <Footer />
     </div>
   )
